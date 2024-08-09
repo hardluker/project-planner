f2ba4f27b6dc80620566aa64e92123f172485156
Donald Azevedo <dba9514@gmail.com>
2024-08-09 16:00:52 -0400

    New view for adding a project

---
+++ src/views/AddProject.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <label>Title:</label>
+    <input type="text" required />
+    <label for=""></label>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
