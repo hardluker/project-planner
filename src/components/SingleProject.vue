4e9c1dd0d9593a183675ce9c06c39f61663e52bf
Donald Azevedo <dba9514@gmail.com>
2024-08-09 13:27:20 -0400

    Added json-server

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3>{{ project.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project']
+};
+</script>
+
+<style></style>
