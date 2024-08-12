24ccb18fdc78ed9640748040f01d3bee03cf6edc
Donald Azevedo <dba9514@gmail.com>
2024-08-12 11:31:03 -0400

    Added model binding

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,9 +1,9 @@
 <template>
   <form @submit.prevent="handleSubmit">
     <label>Title:</label>
-    <input type="text" required />
+    <input type="text" v-model="title" required />
     <label>Details</label>
-    <textarea required></textarea>
+    <textarea v-model="details" required></textarea>
     <button>Add Project</button>
   </form>
 </template>
