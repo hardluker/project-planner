5124426aa4b6780552458b59b3d28046dc848758
Donald Azevedo <dba9514@gmail.com>
2024-08-12 12:58:04 -0400

    Adding a Navbar for navigating to a different page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="main-nav">
+    <router-link :to="{ name: 'home' }">Projects</router-link>
+    <router-link :to="{ name: 'AddProject' }">Add a new Project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
