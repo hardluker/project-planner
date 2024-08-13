5c731429bd84f6feb9c4f4190fa3bf4d37b62896
Donald Azevedo <dba9514@gmail.com>
2024-08-13 14:55:51 -0400

    Built a filter navigation tool

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="filterChange" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject
           :project="project"
           @delete="handleDelete"
@@ -14,19 +15,25 @@
 
 <script>
 import SingleProject from '../components/SingleProject.vue';
+import FilterNav from '@/components/FilterNav.vue';
 
 export default {
   name: 'HomeView',
-  components: { SingleProject },
+  components: { SingleProject, FilterNav },
   data() {
     return {
-      projects: []
+      projects: [],
+      filteredProjects: [],
+      current: 'all'
     };
   },
   mounted() {
     fetch('http://localhost:3000/projects')
       .then((res) => res.json())
-      .then((data) => (this.projects = data))
+      .then((data) => {
+        this.projects = data;
+        this.filteredProjects = this.projects;
+      })
       .catch((err) => console.log(err.message));
   },
   methods: {
@@ -40,6 +47,20 @@ export default {
         return project.id === id;
       });
       project.complete = !project.complete;
+    },
+    filterChange(event) {
+      this.current = event;
+      if (event === 'completed') {
+        this.filteredProjects = this.projects.filter((project) => {
+          return project.complete;
+        });
+      } else if (event === 'ongoing') {
+        this.filteredProjects = this.projects.filter((project) => {
+          return !project.complete;
+        });
+      } else {
+        this.filteredProjects = this.projects;
+      }
     }
   }
 };
